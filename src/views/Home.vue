6c41a511102ceb8b830366bfcbf3e339461a6d7e
faebut <faebutrost@gmail.com>
2019-06-11 17:10:20 +0200

    edited the homepage for a button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <v-container>
     <v-layout>
-      <h1>Willkommen</h1>
+      <h1>Willkommen bei der Benutzerdatenbank des Prozessors. Bitte logge dich ein</h1>
+      <br>
+      <p class="text-xs-center">
+        <v-btn flat large class="success">Login</v-btn>
+      </p>
     </v-layout>
   </v-container>
 </template>
