1a6e72207c2775d1fa1fcb15c0a7b5c2930de764
faebut <faebutrost@gmail.com>
2019-06-12 11:44:40 +0200

    changed login button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="grey--text">Willkommen bei der Benutzerdatenbank des Prozessors. Bitte logge dich ein</h1>
     <v-container class="my-5 text-xs-center">
-      <v-btn large class="success">Login</v-btn>
+      <v-btn large flat class="success">Login</v-btn>
     </v-container>
   </div>
 </template>
