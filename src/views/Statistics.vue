429371eb251038baa0fc446ed1ce53ec51354f7d
faebut <faebutrost@gmail.com>
2020-03-12 11:18:19 +0100

    easier implementation of counting the dates, possibly usable for statistics

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -11,6 +11,10 @@
           </li>
         </ul>
         <div>Total Besuche: {{ allVisits.length }}</div>
+        <h2>Besuche nach Daten:</h2>
+        <ul v-for="(date, index) in visitsPerDate" :key="index">
+          <li>{{ index }} : {{ date }}</li>
+        </ul>
       </v-container>
     </v-layout>
   </v-container>
@@ -54,76 +58,114 @@ export default {
         return b.visits.length - a.visits.length;
       });
     },
-    visitsByDate() {
-      const visitsDate = {};
+    // visitsByDate() {
 
-      for (let y = 2019; y < 2030; y++) {
-        this.allVisits.forEach(visit => {
-          if (visit.date.getFullYear() == y) {
-            if (visitsDate[y]) {
-              for (let m = 0; m < 12; m++) {
-                if (visit.date.getMonth() == m) {
-                  if (visitsDate[y][m]) {
-                    for (let d = 0; d < 30; d++) {
-                      if (visit.date.getDate() == d) {
-                        if (visitsDate[y][m][d]) {
-                          visitsDate[y][m][d].push(visit);
-                        } else {
-                          visitsDate[y][m][d] = [];
-                          visitsDate[y][m][d].push(visit);
-                        }
-                      }
-                    }
-                  } else {
-                    visitsDate[y][m] = {};
-                    for (let d = 0; d < 32; d++) {
-                      if (visit.date.getDate() == d) {
-                        if (visitsDate[y][m][d]) {
-                          visitsDate[y][m][d].push(visit);
-                        } else {
-                          visitsDate[y][m][d] = [];
-                          visitsDate[y][m][d].push(visit);
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            } else {
-              visitsDate[y] = {};
-              for (let m = 0; m < 12; m++) {
-                if (visit.date.getMonth() == m) {
-                  if (visitsDate[y][m]) {
-                    for (let d = 0; d < 30; d++) {
-                      if (visit.date.getDate() == d) {
-                        if (visitsDate[y][m][d]) {
-                          visitsDate[y][m][d].push(visit);
-                        } else {
-                          visitsDate[y][m][d] = [];
-                          visitsDate[y][m][d].push(visit);
-                        }
-                      }
-                    }
-                  } else {
-                    visitsDate[y][m] = {};
-                    for (let d = 0; d < 30; d++) {
-                      if (visit.date.getDate() == d) {
-                        if (visitsDate[y][m][d]) {
-                          visitsDate[y][m][d].push(visit);
-                        } else {
-                          visitsDate[y][m][d] = [];
-                          visitsDate[y][m][d].push(visit);
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        });
+    //   // complicated way to rearrange visitDates in a complex Object
+    //   const visitsDate = {};
+
+    //   for (let y = 2019; y < 2030; y++) {
+    //     this.allVisits.forEach(visit => {
+    //       if (visit.date.getFullYear() == y) {
+    //         if (visitsDate[y]) {
+    //           for (let m = 0; m < 12; m++) {
+    //             if (visit.date.getMonth() == m) {
+    //               if (visitsDate[y][m]) {
+    //                 for (let d = 0; d < 30; d++) {
+    //                   if (visit.date.getDate() == d) {
+    //                     if (visitsDate[y][m][d]) {
+    //                       visitsDate[y][m][d].push(visit);
+    //                     } else {
+    //                       visitsDate[y][m][d] = [];
+    //                       visitsDate[y][m][d].push(visit);
+    //                     }
+    //                   }
+    //                 }
+    //               } else {
+    //                 visitsDate[y][m] = {};
+    //                 for (let d = 0; d < 32; d++) {
+    //                   if (visit.date.getDate() == d) {
+    //                     if (visitsDate[y][m][d]) {
+    //                       visitsDate[y][m][d].push(visit);
+    //                     } else {
+    //                       visitsDate[y][m][d] = [];
+    //                       visitsDate[y][m][d].push(visit);
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             }
+    //           }
+    //         } else {
+    //           visitsDate[y] = {};
+    //           for (let m = 0; m < 12; m++) {
+    //             if (visit.date.getMonth() == m) {
+    //               if (visitsDate[y][m]) {
+    //                 for (let d = 0; d < 30; d++) {
+    //                   if (visit.date.getDate() == d) {
+    //                     if (visitsDate[y][m][d]) {
+    //                       visitsDate[y][m][d].push(visit);
+    //                     } else {
+    //                       visitsDate[y][m][d] = [];
+    //                       visitsDate[y][m][d].push(visit);
+    //                     }
+    //                   }
+    //                 }
+    //               } else {
+    //                 visitsDate[y][m] = {};
+    //                 for (let d = 0; d < 30; d++) {
+    //                   if (visit.date.getDate() == d) {
+    //                     if (visitsDate[y][m][d]) {
+    //                       visitsDate[y][m][d].push(visit);
+    //                     } else {
+    //                       visitsDate[y][m][d] = [];
+    //                       visitsDate[y][m][d].push(visit);
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             }
+    //           }
+    //         }
+    //       }
+    //     });
+    //   }
+    //   return visitsDate;
+    // },
+    visitsPerDate() {
+      let allDates = [];
+
+      this.allVisits.forEach(visit => {
+        allDates.push(visit.date);
+      });
+
+      function pad(n) {
+        return n.toString().length == 1 ? '0' + n : n;
       }
-      return visitsDate;
+
+      function getCount(arr) {
+        var obj = {};
+        for (var i = 0, l = arr.length; i < l; i++) {
+          var thisDate = arr[i];
+          var day = pad(thisDate.getDate());
+          var month = pad(thisDate.getMonth() + 1);
+          var year = thisDate.getFullYear();
+          var key = [year, month, day].join('-');
+          obj[key] = obj[key] || 0;
+          obj[key]++;
+        }
+        return obj;
+      }
+
+      const countDates = getCount(allDates);
+
+      const countDatesOrdered = {};
+      Object.keys(countDates)
+        .sort()
+        .forEach(function(key) {
+          countDatesOrdered[key] = countDates[key];
+        });
+
+      return countDatesOrdered;
     }
   },
   created() {
